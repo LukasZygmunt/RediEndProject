342111ea91f861dc8c5cc81ddcfe2e2cd625fa97
LukasZygmunt <jupsi84@gmail.com>
2023-06-07 13:47:35 +0200

    create "choose directory" function and update readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RediEndProject
+
+# Basic function
+
+The Script show proof weather and choose the best direction to ride.
+
+# now --city
+
+show current weather in the city
+
+# today --city
+
+show today weather in the city and the best direction to cycle
+
+# tomorrow --city
+
+show tomorrow weather in the city and the best direction to cycle
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import argparse
 import json
 import logging
+import sys
 import requests as requests
 
 
@@ -31,7 +33,6 @@ def create_geo(latitude, longitude):
 def create_host(city_geo):
     latitude = city_geo[0]
     longitude = city_geo[1]
-    #host = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation,rain,showers,snowfall,cloudcover,windspeed_10m,winddirection_10m&daily=temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,snowfall_sum,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&forecast_days=3&timezone=Europe%2FBerlin"
     host = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&forecast_days=3&timezone=Europe%2FBerlin"
     return host
 
@@ -45,22 +46,16 @@ def create_file(city: str):
                 return value
     if return_city_r().status_code == 200:
         json.dump(return_city_r().json(), open(file_name, "w", encoding="utf-8"), indent=4, sort_keys=True)
-        print(f"[INFO]File {file_name} is created")
         logging.info(f"[INFO]File {file_name} is created")
     else:
         raise ConnectionError(f"{create_host(city)} replied with {return_city_r().status_code}: {return_city_r().reason}")
     return ""
 
 
-def choose_directory(start_place, when):
-    pass
-
-
 def show_weather(when, where):
     filename = f"{where}.json"
     weather_now = {}
     weather = {}
-    weather_tomorrow = {}
     data = {}
     with open(filename, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -90,35 +85,151 @@ def show_weather(when, where):
         return weather
 
 
+
+def choose_directory(start_place, when):
+    create_file(start_place)
+    create_file("badToelz")
+    create_file("landsbergAmLech")
+    create_file("pfaffenhofenAnDerInn")
+    create_file("haagInOberbayern")
+    start = show_weather(when, start_place)
+    s = show_weather(when, "badToelz")
+    w = show_weather(when, "landsbergAmLech")
+    n = show_weather(when, "pfaffenhofenAnDerInn")
+    e = show_weather(when, "haagInOberbayern")
+
+    weather = {"rain": {"badToelz": s["rain"], "landsbergAmLech": w["rain"], "pfaffenhofenAnDerInn": n["rain"], "haagInOberbayern": e["rain"]},
+               "rain_long": {"badToelz": s["rain_how_long"], "landsbergAmLech": w["rain_how_long"], "pfaffenhofenAnDerInn": n["rain_how_long"], "haagInOberbayern": e["rain_how_long"]},
+               "windspeed": {"badToelz": s["windspeed"], "landsbergAmLech": w["windspeed"], "pfaffenhofenAnDerInn": n["windspeed"], "haagInOberbayern": e["windspeed"]},
+               "winddirection": {"badToelz": s["winddirection"], "landsbergAmLech": w["winddirection"], "pfaffenhofenAnDerInn": n["winddirection"], "haagInOberbayern": e["winddirection"]},
+               "temperature": {"badToelz": s["temperature"], "landsbergAmLech": w["temperature"], "pfaffenhofenAnDerInn": n["temperature"], "haagInOberbayern": e["temperature"]}
+               }
+    # rain_dict = {"badToelz" : s["rain"], "landsbergAmLech" : w["rain"], "pfaffenhofenAnDerInn" :n["rain"], "haagInOberbayern" : e["rain"]}
+    # rain_long_list = {s["rain_how_long"], w["rain_how_long"], n["rain_how_long"], e["rain_how_long"]}
+    # windspeed_list = [start["windspeed"], s["windspeed"], w["windspeed"], n["windspeed"], e["windspeed"]]
+    # winddirection_list = [start["winddirection"], s["winddirection"], w["winddirection"], n["winddirection"], e["winddirection"]]
+    # temperature_list = [start["temperature"], s["temperature"], w["temperature"], n["temperature"], e["temperature"]]
+
+    windspeed_temp = min(weather["windspeed"].values())
+    windspeed_res = [key for key in weather["windspeed"] if weather["windspeed"][key] == windspeed_temp]
+
+    rain_temp = min(weather["rain"].values())
+    rain_res = [key for key in weather["rain"] if weather["rain"][key] == rain_temp]
+
+    rain_long_temp = min(weather["rain_long"].values())
+    rain_long_res = [key for key in weather["rain_long"] if weather["rain_long"][key] == rain_long_temp]
+
+    temperature_temp = max(weather["temperature"].values())
+    temperature_res = [key for key in weather["temperature"] if weather["temperature"][key] == temperature_temp]
+
+    def return_same_wind():
+        def return_direction(winddirection):
+            if ((winddirection > 0 and winddirection <= 45) or (winddirection > 315)):
+                direction = "n"
+            elif (winddirection > 45 and winddirection <= 135):
+                direction = "e"
+            elif (winddirection > 135 and winddirection <= 225):
+                direction = "s"
+            elif (winddirection > 225 and winddirection <= 315):
+                direction = "w"
+            return direction
+
+        if return_direction(start["winddirection"]) == return_direction(s["winddirection"]):
+           city = "badToelz"
+        elif return_direction(start["winddirection"]) == return_direction(w["winddirection"]):
+           city = "landsbergAmLech"
+        elif return_direction(start["winddirection"]) == return_direction(n["winddirection"]):
+           city = "pfaffenhofenAnDerInn"
+        elif return_direction(start["winddirection"]) == return_direction(e["winddirection"]):
+           city = "haagInOberbayern"
+        return city
+    winddirection_res = []
+    winddirection_res.append(return_same_wind())
+    res = []
+    if len(rain_res) > 1:
+        if len(rain_long_res) > 1:
+            if set(winddirection_res) & set(rain_long_res):
+                res =  winddirection_res
+            elif set(windspeed_res) & set(rain_long_res):
+                res = windspeed_res
+            elif set(temperature_res) & set(rain_long_res):
+                res = temperature_res
+            else:
+                res = rain_long_res
+        else:
+            res = rain_long_res
+    else:
+        res = rain_res
+    for r in res:
+        return f"The best direction to cycle {when} is/are {r}"
+
+
+
+
+
 # run
 if __name__ == "__main__":
-    pass
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s (%(filename)s)")
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
 
-# geo
-munich = (48.15, 11.42)
-badToelz = (47.76, 11.56)
-landsbergAmLech = (48.05, 10.88)
-pfaffenhofenAnDerInn = (48.53, 11.51)
-haagInOberbayern = (48.16, 12.18)
+    logging.info("starting up")
 
-#requests
-munich_r = requests.get(create_host(munich))
-badToelz_r = requests.get(create_host(badToelz))
-landsbergAmLech_r = requests.get(create_host(landsbergAmLech))
-pfaffenhofenAnDerInn_r = requests.get(create_host(pfaffenhofenAnDerInn))
-haagInOberbayern_r = requests.get(create_host(haagInOberbayern))
+    parser = argparse.ArgumentParser(
+        description=("parse the action")
+    )
 
-# lists and dictionaries
-city_requests_list = [munich_r, badToelz_r, landsbergAmLech_r, pfaffenhofenAnDerInn_r, haagInOberbayern_r]
-city_requests_dict = {"munich" : munich_r,
-                      "badToelz" : badToelz_r,
-                      "landsbergAmLech" : landsbergAmLech_r,
-                      "pfaffenhofenAnDerInn" : pfaffenhofenAnDerInn_r,
-                      "haagInOberbayern" : haagInOberbayern_r
-                      }
+    parser.add_argument(
+        "action",
+        type=str,
+        help="options: (now|today|tomorrow)",
+    )
+    parser.add_argument(
+        "--city",
+        type=str,
+        help=("a string representing city ")
+    )
+    args = parser.parse_args()
+    action = args.action
+    if args.city:
+        city = args.city
 
-# print(create_file("munich"))
-# add_to_dict()
-# print_dict(city_requests_dict)
-#
-print(show_weather("today", "munich"))
+    if action == "now":
+        if not args.city:
+            logging.error("no --city")
+            exit()
+        city_1 = city
+        show_weather("now", city_1)
+
+
+    # geo
+    munich = (48.15, 11.42)
+    badToelz = (47.76, 11.56)
+    landsbergAmLech = (48.05, 10.88)
+    pfaffenhofenAnDerInn = (48.53, 11.51)
+    haagInOberbayern = (48.16, 12.18)
+
+    #requests
+    munich_r = requests.get(create_host(munich))
+    badToelz_r = requests.get(create_host(badToelz))
+    landsbergAmLech_r = requests.get(create_host(landsbergAmLech))
+    pfaffenhofenAnDerInn_r = requests.get(create_host(pfaffenhofenAnDerInn))
+    haagInOberbayern_r = requests.get(create_host(haagInOberbayern))
+
+    # lists and dictionaries
+    city_requests_list = [munich_r, badToelz_r, landsbergAmLech_r, pfaffenhofenAnDerInn_r, haagInOberbayern_r]
+    city_requests_dict = {"munich" : munich_r,
+                          "badToelz" : badToelz_r,
+                          "landsbergAmLech" : landsbergAmLech_r,
+                          "pfaffenhofenAnDerInn" : pfaffenhofenAnDerInn_r,
+                          "haagInOberbayern" : haagInOberbayern_r
+                          }
+
+
+
+    print(show_weather("today", "munich"))
+    print(choose_directory("munich", "today"))
